a8116d20695edf2cfacb23eb71f9e9a24230ae1c
nadiaaiello <nadialucianaaiello@gmail.com>
2023-11-15 10:09:44 -0600

    Arreglado CSS+JS

--- pagina web/js/alumnos.js
+++ pagina web/js/alumnos.js
@@ -22,9 +22,9 @@ const alumnos = [
         
     },
     {
-        "apellido": "Iannello",
-        "nombre": "María Cecilia",
-        "titulo":"Diseñadora Multimedial",
+        "apellido": "Aiello",
+        "nombre": "Nadia",
+        "titulo":"Desarrolladora Front-End",
         "descripcion":"Me especializo en Desarrollo...",
         "cv": "url al cv",
         "correo":"mciannello@janer.esc.edu.ar",
@@ -42,26 +42,29 @@ var tarjeta= document.getElementById("tarjeta")
 
 alumnos.forEach(alumno => {
     console.log (alumno)
-    tarjeta.innerHTML= "<div class='card mb-3' style='max-width: 540px;'>"+
-   " <div class='row g-0'>"+
-      "<div class='col-md-4'>"+
-        "<img src='./img/"+alumno.imagen+"' class='img-fluid rounded-start' alt='Foto'>"+
-      "</div>"+
-      "<div class='col-md-8'>"+
-        "<div class='card-body'>"+
-          "<h5 class='card-title' id='titulo'>"+alumno.apellido+"  "+alumno.nombre+"</h5>"+
-          "<h6 class='card-subtitle>"+alumno.titulo+"</h6>"+
-         " <p class='card-text'>"+alumno.descripcion+"</p>"+
-          "<div class='card-body'>"+
-           " <a href='"+alumno.cv+"'' class='card-link'>CV</a>"+
-           " <a href='"+alumno.correo+"' class='card-link'>Correo</a>"+
-         " </div>"+
-       " </div>"+
-        "</div>"+
-      "</div>"+
-    "</div>"+
-  "</div>"
+    let nuevoAlumno=document.createElement("div")
+
+    nuevoAlumno.innerHTML= "<div class='card mb-3' style='max-width: 540px;'>"+
+                            " <div class='row g-0'>"+
+                              "<div class='col-md-4'>"+
+                                "<img src='./img/alumnos/"+alumno.imagen+"' class='img-fluid rounded-start' alt='Foto'>"+
+                              "</div>"+
+                              "<div class='col-md-8'>"+
+                                "<div class='card-body'>"+
+                                  "<h5 class='card-title' id='titulo'>"+alumno.apellido+"  "+alumno.nombre+"</h5>"+
+                                  "<h6 class='card-subtitle>"+alumno.titulo+"</h6>"+
+                                  " <p class='card-text'>"+alumno.descripcion+"</p>"+
+                                  "<div class='card-body'>"+
+                                    " <a href='"+alumno.cv+"'' class='card-link'>CV</a>"+
+                                    " <a href='"+alumno.correo+"' class='card-link'>Correo</a>"+
+                                  " </div>"+
+                                " </div>"+
+                                "</div>"+
+                              "</div>"+
+                            "</div>"+
+                          "</div>"
   
     // apellido.innerHTML= alumno.apellido + "  "+ alumno.nombre
+    tarjeta.appendChild(nuevoAlumno)
     
 });
